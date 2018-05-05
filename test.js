0ea8ca0b11d8f334d60af143d3b2633167590165
David Waller <david@krawaller.se>
2018-05-05 14:28:52 +0200

    upgrade callbag-mock and test

--- test.js
+++ test.js
@@ -3,12 +3,9 @@ const makeMockCallbag = require('callbag-mock');
 const latest = require('./index');
 
 test('it emits the latest from listenable when downstream source asks', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && d !== undefined && history.push([name,dir,t,d]);
-
-  const listenable = makeMockCallbag('source', true);
+  const listenable = makeMockCallbag(true);
   const pullable = latest(listenable);
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   pullable(0, sink);
 
@@ -19,60 +16,49 @@ test('it emits the latest from listenable when downstream source asks', t => {
   sink.emit(1);
   listenable.emit(1, 'bin');
 
-  t.deepEqual(history, [
-    ['sink', 'body', 1, 'bar'],
-    ['sink', 'body', 1, 'baz'],
-  ], 'sink gets only what it asks for');
+  t.deepEqual(
+    sink.getReceivedData(),
+    ['bar', 'baz'],
+    'sink gets only what it asks for'
+  );
 
   t.end();
 });
 
 test('it terminates the listenable if terminated from downstream', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const listenable = makeMockCallbag('source', report, true);
+  const listenable = makeMockCallbag(true);
   const pullable = latest(listenable);
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   pullable(0, sink);
 
   sink.emit(2);
 
-  t.deepEqual(history, [
-    ['source', 'talkback', 2, undefined],
-  ], 'listenable is terminated');
+  t.ok(!listenable.checkConnection(),'listenable is terminated');
 
   t.end();
 });
 
 test('it terminates downstream if listenable terminates', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && d !== undefined && history.push([name,dir,t,d]);
 
-  const listenable = makeMockCallbag('source', true);
+  const listenable = makeMockCallbag(true);
   const pullable = latest(listenable);
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   pullable(0, sink);
 
   listenable.emit(1, 'foo');
   listenable.emit(2, 'error');
 
-  t.deepEqual(history, [
-    ['sink', 'body', 2, 'error'],
-  ], 'sink receives termination event');
+  t.ok(!sink.checkConnection(),'listenable is terminated');
 
   t.end();
 });
 
 test('it replies nothing if we havent gotten any latest data', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && !(t===1 && d===0) && history.push([name,dir,t,d]);
-
-  const listenable = makeMockCallbag('source', true);
+  const listenable = makeMockCallbag(true);
   const pullable = latest(listenable);
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   pullable(0, sink);
 
@@ -81,9 +67,11 @@ test('it replies nothing if we havent gotten any latest data', t => {
   listenable.emit(1, 'bar');
   sink.emit(1);
 
-  t.deepEqual(history, [
-    ['sink', 'body', 1, 'bar']
-  ], 'sink only gets reply when there is data to get');
+  t.deepEqual(
+    sink.getReceivedData(),
+    ['bar'],
+    'sink only gets reply when there is data to get'
+  );
 
   t.end();
 });
